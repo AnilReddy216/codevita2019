7050066bf90a933023a050ceebe0234c45febc6c
Aarya <53687608+aaryalanchaster@users.noreply.github.com>
2019-08-07 22:53:56 +0530

    bottlenecks.java

--- bottlenecks.java
+++ bottlenecks.java
@@ -1,52 +1,50 @@
-
 import java.util.*;
-class bottles
+class Bottles
 {
-    public void bottle(int a[],int n )
+    public static void NumofBottle(int arr[],int n )
     {
         int max=0;
         for(int i=0;i<n;i++)
         {
-            if (a[i]>max)
+            if (arr[i]>max)
             {
-                max=a[i];
+                max=arr[i];
             }
         }
         int freq[]=new int[max+1];
         for(int i=0;i<n;i++)
         {
-            freq[a[i]]++;
+            freq[arr[i]]++;
         }
-        int f=0;
+        int bottles=0;
         for(int i=0;i<max+1;i++)
         {
-            if (freq[i]>f)
+            if (freq[i]>bottles)
             {
-                f=freq[i];
+                bottles=freq[i];
             }
         }
 
-        System.out.println(f);
+        System.out.println("Bottles:"+bottles);
     }
 }
-class geeks{
+class Bottlenecks
+{
     public static void main(String [] args)
     {
-    Scanner sc = new Scanner(System.in);
-    int tc=sc.nextInt();
-    while(tc-- >0)
-    {
-        System.out.println("enter the total bottles");
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the total bottles:");
         int n=sc.nextInt();
-        System.out.println("enter the radius:");
-        int a[]=new int[n];
+        System.out.println("Enter the radius:");
+        int arr[]=new int[n];
         for(int i=0;i<n;i++)
         {
-            a[i]=sc.nextInt();
+            arr[i]=sc.nextInt();
         }
 
-        bottles b=new bottles();
-        b.bottle(a,n);
+        Bottles obj=new Bottles();
+        obj.NumofBottle(arr,n);
+        sc.close();
     }
-    }
+    
 }
